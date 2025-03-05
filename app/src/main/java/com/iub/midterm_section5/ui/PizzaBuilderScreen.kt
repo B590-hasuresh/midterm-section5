1602786a0e5ed1b4975eb2761e8b359f9a8356ec
subavenk <subavenk@iu.edu>
2025-03-04 20:55:27 -0500

    Completed Midterm Section5 Part2

--- app/src/main/java/com/iub/midterm_section5/ui/PizzaBuilderScreen.kt
+++ app/src/main/java/com/iub/midterm_section5/ui/PizzaBuilderScreen.kt
@@ -1,5 +1,7 @@
-package com.iub.midterm_section5.ui.theme
+package com.iub.midterm_section5.ui
 
+import Pizza
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,14 +10,19 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.iub.midterm_section5.R
-import com.iub.midterm_section5.iu.ToppingCell
 import com.iub.midterm_section5.model.Topping
 import com.iub.midterm_section5.model.ToppingPlacement
+import java.text.NumberFormat
 import java.util.Locale
 
 @Preview
@@ -24,16 +31,21 @@ fun PizzaBuilderScreen(
     modifier: Modifier = Modifier
 ) {
 
+    var pizza by rememberSaveable { mutableStateOf(Pizza()) }
+
     Column(
         modifier = modifier
     ) {
         ToppingsList(
+            pizza = pizza,
+            onEditPizza = {pizza = it},
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f, fill = true)
         )
 
         OrderButton(
+            pizza = pizza,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
@@ -45,26 +57,44 @@ fun PizzaBuilderScreen(
 
 @Composable
 private fun ToppingsList(
+    pizza: Pizza,
+    onEditPizza: (Pizza) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var toppingBeingAdded by rememberSaveable { mutableStateOf<Topping?>(null) }
+
+    if (toppingBeingAdded != null) {
+        toppingBeingAdded?.let { topping ->
+            ToppingPlacementDialog(
+                topping = topping,
+                onSetToppingPlacement = { placement ->
+                    onEditPizza(pizza.withTopping(topping, placement))
+                },
+                onDismissRequest = {
+                    toppingBeingAdded = null
+                }
+            )
+        }
+    }
+
     LazyColumn(
         modifier = modifier
     ) {
         items(Topping.values()) { topping ->
             ToppingCell(
                 topping = topping,
-                placement = ToppingPlacement.Left,
+                placement = pizza.toppings[topping],
                 onClickTopping = {
-                    // TODO
+                    toppingBeingAdded = topping
                 }
             )
         }
     }
-
 }
 
 @Composable
 private fun OrderButton(
+    pizza: Pizza,
     modifier: Modifier = Modifier
 ) {
     Button(
@@ -73,8 +103,11 @@ private fun OrderButton(
             // TODO
         }
     ) {
+        val currencyFormatter = remember { NumberFormat.getCurrencyInstance() }
+        val price = currencyFormatter.format(pizza.price)
+
         Text(
-            text = stringResource(R.string.place_order_button)
+            text = stringResource(R.string.place_order_button, price)
                 .toUpperCase(Locale.getDefault())
         )
     }
